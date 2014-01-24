85e94036c5cdd50869531f8768b2a3dd4e9e8213
Jason Robertson <jayperdu@gmail.com>
2014-01-24 14:01:43 -0700

    Remove Foreground Lettering on Christmas Chest

--- acm/christmasChest/GuiChristmasChest.java
+++ acm/christmasChest/GuiChristmasChest.java
@@ -33,15 +33,6 @@ public class GuiChristmasChest extends GuiContainer {
     }
 
     /**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items)
-     */
-    protected void drawGuiContainerForegroundLayer(int par1, int par2)
-    {
-        this.fontRenderer.drawString(this.lowerChestInventory.isInvNameLocalized() ? this.lowerChestInventory.getInvName() : I18n.getString(this.lowerChestInventory.getInvName()), 8, 6, 4210752);
-        this.fontRenderer.drawString(this.upperChestInventory.isInvNameLocalized() ? this.upperChestInventory.getInvName() : I18n.getString(this.upperChestInventory.getInvName()), 8, this.ySize - 96 + 2, 4210752);
-    }
-
-    /**
      * Draw the background layer for the GuiContainer (everything behind the items)
      */
     protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
